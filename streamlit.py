1d9f1ba88e40a018fb79490d8fd03cdfc377c7a5
도형준 <105966480+dorae222@users.noreply.github.com>
2022-11-29 10:42:31 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -190,12 +190,10 @@ elif choose == "Predicting":
                 단지내주차면수 = r3_col1.slider("단지내주차면수",13,1798)
                 공급유형_비율 = r3_col2.slider("공급유형_비율",0,60)
                 지역_비율 = r3_col3.slider("지역_비율",0,21)
-                predict_button = st.button("예측")
-                
-                if predict_button:
-                        variable = np.array([총세대수, 전용면적, 전용면적별세대수, 공가수, 지하철, 버스, 단지내주차면수, 공급유형_비율, 지역_비율])
-                        model = joblib.load('LinearRegression.pkl')
-                        pred = model.predict([variable])
+
+                variable = np.array([총세대수, 전용면적, 전용면적별세대수, 공가수, 지하철, 버스, 단지내주차면수, 공급유형_비율, 지역_비율])
+                model = joblib.load('LinearRegression.pkl')
+                pred = model.predict([variable])
                 
                 ##############
                 
@@ -219,11 +217,9 @@ elif choose == "Predicting":
                 공급유형_비율 = r3_col2.slider("공급유형_비율.",0,60)
                 지역_비율 = r3_col3.slider("지역_비율.",0,21)
                 
-                predict_button = st.button("예측")
-                if predict_button:
-                        variable = np.array([총세대수, 전용면적, 전용면적별세대수, 공가수, 지하철, 버스, 단지내주차면수, 공급유형_비율, 지역_비율])
-                        model = joblib.load('lightgbm.pkl')
-                        pred = model.predict([variable])
+                variable = np.array([총세대수, 전용면적, 전용면적별세대수, 공가수, 지하철, 버스, 단지내주차면수, 공급유형_비율, 지역_비율])
+                model = joblib.load('lightgbm.pkl')
+                pred = model.predict([variable])
                 
                 #########
                 
