29c9dc6f457968cb8263a08d60517b8f6acaf4b7
SYHWANG0629 <117613957+SYHWANG0629@users.noreply.github.com>
2022-11-29 12:46:59 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -172,8 +172,13 @@ elif choose == "Visualizing":
         
         df_2 = pd.read_csv('age_gender_info.csv')
         df_2 = df_2.groupby('지역').mean()
-        
+
         fig_3 = px.bar(df_2, x = df_2.index, y = df_2.columns, )
+        fig.update_layout({"title": {"text": "지역별 세대(성별/연령)","x": 0.5, "y": 0.9, # y 축 기준 타이틀 위치
+                                     "font": {"size": 20 # 타이틀 글씨 크기}},"showlegend": True,"xaxis": 
+                                              {"title": "지역","showticklabels": True, "dtick": 1 # x 축 간격 범위},
+                                               "autosize":False,"width": 800,"height": 400})
+        
 
         st.plotly_chart(fig_3)
 
